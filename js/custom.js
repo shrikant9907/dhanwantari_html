cf02ca154e2ef980c1b92762b7003fb863f1383e
Shrikant Yadav <shrikant9907@gmail.com>
2019-08-09 16:01:23 +0000

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,83 @@
+jQuery('document').ready(function(){
+
+    var eventInfo = [
+         "for modern EROs",
+         "be efficient and increase your bottom line",
+         "track your referrals",
+         "gain full control of your business",
+         "track each marketing dollar!",
+         "extensive useful reporting ",
+         "offer interest free, 0% APR advances to all your customer!",
+         "bank level security",
+         "use your own funding",
+         "for EROs by EROs"
+         ];
+
+	 var typed5 = new Typed('#typed5', {
+	    strings: eventInfo,
+        typeSpeed: 50,
+        backSpeed: 40,
+        backDelay: 2000,
+        loop: true 
+	  });
+ 
+	jQuery('.testimonials_slick').slick();
+
+	jQuery('.hamburger_sign').on('click',function(){
+		jQuery('.hover_menus').toggle();
+	});
+
+	jQuery('.hover_menus').hover(function(){
+		jQuery(this).show();
+	}, function(){
+		jQuery(this).hide();
+	});  
+
+	jQuery('.three_col_carosusel').slick({ 
+		infinite: true,
+		speed: 300,
+		slidesToShow: 3,
+		centerMode: true,
+	 	centerPadding: '0',
+		slidesToScroll: 3,
+		responsive: [ 
+		{
+		  breakpoint: 1024,
+		  settings: {
+		    slidesToShow: 3,
+		    slidesToScroll: 3,
+		    infinite: true,
+		  }
+		},
+		{
+		  breakpoint: 600,
+		  settings: {
+		    slidesToShow: 2,
+		    slidesToScroll: 2
+		  }
+		},
+		{
+		  breakpoint: 480,
+		  settings: {
+		    slidesToShow: 1,
+		    slidesToScroll: 1
+		  }
+		}
+		]
+	});
+
+});
+
+ 
+
+   
+jQuery(window).scroll(function() {    
+    if ($(window).width() > 767) {
+	   var scroll = $(window).scrollTop();
+       if (scroll >= 580) {
+	        $("body").addClass("scrolled");
+	    } else {
+	        $("body").removeClass("scrolled");
+	    }
+	}  
+}); 
